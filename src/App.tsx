e25b27b3499cb9c0393de829c2205c1f93740b13
Ahmed Yosry <amu3dev@gmail.com>
2023-03-29 14:04:54 +0200

    - Applied with the 2 ways in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
-import "./App.css";
+import Todos from "./components/Todos";
+import To from "./components/To";
+import Todo from "./models/todo";
+
+const todos: Todo[] = [
+  { id: "1", title: "Learn TypeScript", completed: true },
+  { id: "2", title: "Learn React", completed: true },
+  { id: "3", title: "Learn Redux", completed: false },
+  { id: "4", title: "Learn GraphQL", completed: false },
+  
+];
 
 function App() {
-  return <div></div>;
+  const head = "To do list";
+
+  return (
+    <div>
+      <Todos items={todos} head={head} />
+      <To items={todos} head={head} />
+    </div>
+  );
 }
 
 export default App;
