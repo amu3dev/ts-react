bf6802cd5dc2576937ba7e9fc12d288d3ab5cecf
Ahmed Yosry <amu3dev@gmail.com>
2023-03-30 01:11:09 +0200

    - Call <NewTodo />

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Todos from "./components/Todos";
 import To from "./components/To";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 const todos: Todo[] = [
   { id: "1", title: "Learn TypeScript", completed: true },
   { id: "2", title: "Learn React", completed: true },
   { id: "3", title: "Learn Redux", completed: false },
   { id: "4", title: "Learn GraphQL", completed: false },
-  
 ];
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
 
   return (
     <div>
+      <NewTodo />
       <Todos items={todos} head={head} />
       <To items={todos} head={head} />
     </div>
