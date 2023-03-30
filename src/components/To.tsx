70d003fa8a918cd47e362aa43ada8914083b68f8
Ahmed Yosry <amu3dev@gmail.com>
2023-03-30 03:42:58 +0200

    Special handleClick and onDelete (liftup)

--- src/components/To.tsx
+++ src/components/To.tsx
@@ -3,14 +3,20 @@ import Todo from "../models/todo";
 interface ToProps {
   items: Todo[];
   head: string;
+  onDelete: (title: string) => void;
 }
-const To = ({ items, head }: ToProps) => {
+const To = ({ items, head, onDelete }: ToProps) => {
+  const handleClick = (title: string) => {
+    console.log("Clicked");
+    onDelete(title);
+  };
+
   return (
     <div>
       <div>{head}</div>
       <ul>
         {items.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} onClick={() => handleClick(item.title)}>
             {item.title} {item.completed ? "✅" : "❌"}
           </li>
         ))}
