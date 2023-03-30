2ce3027add5921ac850ea693f58f52a21e64b3cb
Ahmed Yosry <amu3dev@gmail.com>
2023-03-30 03:41:54 +0200

    Adeed onDelete (liftup) and handelClick

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,12 @@
-const TodoItem: React.FC<{ title: string }> = ({ title }) => {
-  return <li>{title}</li>;
+const TodoItem: React.FC<{
+  title: string;
+  onDelete: (title: string) => void;
+}> = ({ title, onDelete }) => {
+  const handelClick = () => {
+    // console.log(title);
+    onDelete(title);
+  };
+  return <li onClick={handelClick}>{title}</li>;
 };
 
 export default TodoItem;
