9d69e9d687457a887044047a532b355c71539d0e
Ahmed Yosry <amu3dev@gmail.com>
2023-03-30 03:44:20 +0200

    useRef hook and onSubmit

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import { useRef } from "react";
 
-const NewTodo: React.FC<{}> = () => {
+const NewTodo: React.FC<{ onSubmit: (text: string) => void }> = ({
+  onSubmit,
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handelSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    const enteredText = inputRef.current!.value;
+    //console.log(enteredText);
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+    onSubmit(enteredText);
   };
+
   return (
     <form onSubmit={handelSubmit}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={inputRef} />
       <button type="submit">ADD TODO</button>
     </form>
   );
